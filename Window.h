83983ba924db09d52860147d232cbfba3261e3fc
Dominik Lameter <dominik@lameter.com>
2020-07-20 12:04:27 -0500

    Added tooltip.h include as well as moved to using Qt's namespace macros. Added tooltips and tooltip private members.

--- Window.h
+++ Window.h
@@ -2,9 +2,16 @@
 #define WINDOW_H
 
 #include "DataRetriever.h"
+#include "tooltip.h"
 #include <string>
 #include <QtCharts>
 #include <QMainWindow>
+
+QT_CHARTS_BEGIN_NAMESPACE
+class QChart;
+QT_CHARTS_END_NAMESPACE
+
+QT_USE_NAMESPACE
 
 class Window : public QMainWindow {
 
@@ -16,7 +23,9 @@ public:
 
 private:
     DataRetriever retriever;
-    QtCharts::QChartView* chartview;
+    QChartView* chartview;
+    Tooltip* tooltip;
+    QList<Tooltip*> tooltips;
 };
 
 #endif
