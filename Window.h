28338057c54aa17ebe805a99e9f1ffeae1404565
Dominik Lameter <dominik@lameter.com>
2020-07-20 12:12:58 -0500

    Changed window parent to be graphicsview to better support the tooltip functionality. Changed private member variable names and added chart as a private member to Window. Added Qt slots for tooltip functionality.

--- Window.h
+++ Window.h
@@ -3,9 +3,11 @@
 
 #include "DataRetriever.h"
 #include "tooltip.h"
-#include <string>
-#include <QtCharts>
-#include <QMainWindow>
+#include <QtCharts/QChartGlobal>
+#include <QtWidgets/QGraphicsView>
+
+QT_BEGIN_NAMESPACE
+QT_END_NAMESPACE
 
 QT_CHARTS_BEGIN_NAMESPACE
 class QChart;
@@ -13,19 +15,23 @@ QT_CHARTS_END_NAMESPACE
 
 QT_USE_NAMESPACE
 
-class Window : public QMainWindow {
+class Window : public QGraphicsView {
 
     Q_OBJECT
 
 public:
 
-    Window();
+    Window(QWidjet* parent = 0);
+
+public slots:
+    void keepCallout();
+    void tooltip(QPointF point, bool state);
 
 private:
-    DataRetriever retriever;
-    QChartView* chartview;
-    Tooltip* tooltip;
-    QList<Tooltip*> tooltips;
+    DataRetriever m_retriever;
+    QChart* m_chart;
+    Tooltip* m_tooltip;
+    QList<Tooltip*> m_tooltips;
 };
 
 #endif
